8ed5c3bf3255b273f72a279124911d0b8eeeca35
Carl <39366974+CarlFlo@users.noreply.github.com>
2026-09-14 20:45:36 +0200

    Scope targeted live updates

--- internal/jobs/jobs.go
+++ internal/jobs/jobs.go
@@ -15,7 +15,7 @@ type Service struct {
 	Metadata      MetadataSource
 	Control       ProviderControl
 	Backup        BackupCreator
-	OnChange      func(...string)
+	OnChange      func(string, ...string)
 	ctx           context.Context
 	cancel        context.CancelFunc
 	mu            sync.Mutex
