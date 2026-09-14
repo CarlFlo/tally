9373e0f99d01da63656d38bde822d46f7b683dc8
Carl <39366974+CarlFlo@users.noreply.github.com>
2026-09-14 20:45:44 +0200

    Scope targeted live updates

--- internal/metadata/changes.go
+++ internal/metadata/changes.go
@@ -2,6 +2,12 @@ package metadata
 
 func (s *Service) changed(resources ...string) {
 	if s.OnChange != nil {
-		s.OnChange(resources...)
+		s.OnChange("", resources...)
+	}
+}
+
+func (s *Service) changedProfile(profile string, resources ...string) {
+	if s.OnChange != nil {
+		s.OnChange(profile, resources...)
 	}
 }
